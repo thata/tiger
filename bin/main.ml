1b9523dc8aa3e7f6250b431d8d42dce502c36994
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2023-08-25 11:23:32 +0900

    oops

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,7 @@
 let () =
   let buff = Lexing.from_channel stdin in
   let expr = Tiger.Parser.program Tiger.Lexer.token buff in
-  let result = Tiger.Eval.f expr in
+  let result = Tiger.Eval.f expr [] in
   print_string "Result: ";
   Tiger.Eval.print_val result;
   print_newline ()
